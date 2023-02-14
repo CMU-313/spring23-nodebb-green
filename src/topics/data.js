657619b8ca0af5ace5d11846e72f06ae665fddc9
Michelle <mliuu3259@gmail.com>
2023-02-13 23:10:30 -0500

    modify boolean fields

--- src/topics/data.js
+++ src/topics/data.js
@@ -15,6 +15,10 @@ const intFields = [
     'deleterUid',
 ];
 
+const booleanFields = [
+    'resolve',
+];
+
 module.exports = function (Topics) {
     Topics.getTopicsFields = async function (tids, fields) {
         if (!Array.isArray(tids) || !tids.length) {
@@ -96,6 +100,7 @@ function modifyTopic(topic, fields) {
     }
 
     db.parseIntFields(topic, intFields, fields);
+    db.parseBooleanFields(topic, booleanFields, fields);
 
     if (topic.hasOwnProperty('title')) {
         topic.titleRaw = topic.title;
