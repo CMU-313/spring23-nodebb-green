bb9b63f939c1f2f868d44cc94010410b33464a46
Esther Bae <ohnyoo.bae@gmail.com>
2023-02-20 18:17:38 -0500

    created ts file, fixed imports

--- src/api/topics.js
+++ src/api/topics.js
@@ -1,161 +1,169 @@
-'use strict';
-
-const user = require('../user');
-const topics = require('../topics');
-const posts = require('../posts');
-const meta = require('../meta');
-const privileges = require('../privileges');
-
-const apiHelpers = require('./helpers');
-
+"use strict";
+// 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = require("../user");
+const topics = require("../topics");
+const posts = require("../posts");
+const meta = require("../meta");
+const privileges = require("../privileges");
+const apiHelpers = require("./helpers");
+const websockets = require("../socket.io");
+const socketHelpers = require("../socket.io/helpers");
 const { doTopicAction } = apiHelpers;
-
-const websockets = require('../socket.io');
-const socketHelpers = require('../socket.io/helpers');
-
 const topicsAPI = module.exports;
-
-topicsAPI.get = async function (caller, data) {
-    const [userPrivileges, topic] = await Promise.all([
-        privileges.topics.get(data.tid, caller.uid),
-        topics.getTopicData(data.tid),
-    ]);
-    if (
-        !topic ||
-        !userPrivileges.read ||
-        !userPrivileges['topics:read'] ||
-        !privileges.topics.canViewDeletedScheduled(topic, userPrivileges)
-    ) {
-        return null;
-    }
-
-    return topic;
-};
-
-topicsAPI.create = async function (caller, data) {
-    if (!data) {
-        throw new Error('[[error:invalid-data]]');
-    }
-
-    const payload = { ...data };
-    payload.tags = payload.tags || [];
-    apiHelpers.setDefaultPostData(caller, payload);
-    const isScheduling = parseInt(data.timestamp, 10) > payload.timestamp;
-    if (isScheduling) {
-        if (await privileges.categories.can('topics:schedule', data.cid, caller.uid)) {
-            payload.timestamp = parseInt(data.timestamp, 10);
-        } else {
-            throw new Error('[[error:no-privileges]]');
+topicsAPI.get = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const [userPrivileges, topic] = yield Promise.all([
+            privileges.topics.get(data.tid, caller.uid),
+            topics.getTopicData(data.tid),
+        ]);
+        if (!topic ||
+            !userPrivileges.read ||
+            !userPrivileges['topics:read'] ||
+            !privileges.topics.canViewDeletedScheduled(topic, userPrivileges)) {
+            return null;
         }
-    }
-
-    await meta.blacklist.test(caller.ip);
-    const shouldQueue = await posts.shouldQueue(caller.uid, payload);
-    if (shouldQueue) {
-        return await posts.addToQueue(payload);
-    }
-
-    const result = await topics.post(payload);
-    await topics.thumbs.migrate(data.uuid, result.topicData.tid);
-
-    socketHelpers.emitToUids('event:new_post', { posts: [result.postData] }, [caller.uid]);
-    socketHelpers.emitToUids('event:new_topic', result.topicData, [caller.uid]);
-    socketHelpers.notifyNew(caller.uid, 'newTopic', { posts: [result.postData], topic: result.topicData });
-
-    return result.topicData;
-};
-
-topicsAPI.reply = async function (caller, data) {
-    if (!data || !data.tid || (meta.config.minimumPostLength !== 0 && !data.content)) {
-        throw new Error('[[error:invalid-data]]');
-    }
-    const payload = { ...data };
-    apiHelpers.setDefaultPostData(caller, payload);
-
-    await meta.blacklist.test(caller.ip);
-    const shouldQueue = await posts.shouldQueue(caller.uid, payload);
-    if (shouldQueue) {
-        return await posts.addToQueue(payload);
-    }
-
-    const postData = await topics.reply(payload); // postData seems to be a subset of postObj, refactor?
-    const postObj = await posts.getPostSummaryByPids([postData.pid], caller.uid, {});
-
-    const result = {
-        posts: [postData],
-        'reputation:disabled': meta.config['reputation:disabled'] === 1,
-        'downvote:disabled': meta.config['downvote:disabled'] === 1,
-    };
-
-    user.updateOnlineUsers(caller.uid);
-    if (caller.uid) {
-        socketHelpers.emitToUids('event:new_post', result, [caller.uid]);
-    } else if (caller.uid === 0) {
-        websockets.in('online_guests').emit('event:new_post', result);
-    }
-
-    socketHelpers.notifyNew(caller.uid, 'newPost', result);
-
-    return postObj[0];
-};
-
-topicsAPI.delete = async function (caller, data) {
-    await doTopicAction('delete', 'event:topic_deleted', caller, {
-        tids: data.tids,
+        return topic;
     });
 };
-
-topicsAPI.restore = async function (caller, data) {
-    await doTopicAction('restore', 'event:topic_restored', caller, {
-        tids: data.tids,
+topicsAPI.create = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!data) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        const payload = Object.assign({}, data);
+        payload.tags = payload.tags || [];
+        apiHelpers.setDefaultPostData(caller, payload);
+        const isScheduling = parseInt(data.timestamp, 10) > payload.timestamp;
+        if (isScheduling) {
+            if (yield privileges.categories.can('topics:schedule', data.cid, caller.uid)) {
+                payload.timestamp = parseInt(data.timestamp, 10);
+            }
+            else {
+                throw new Error('[[error:no-privileges]]');
+            }
+        }
+        yield meta.blacklist.test(caller.ip);
+        const shouldQueue = yield posts.shouldQueue(caller.uid, payload);
+        if (shouldQueue) {
+            return yield posts.addToQueue(payload);
+        }
+        const result = yield topics.post(payload);
+        yield topics.thumbs.migrate(data.uuid, result.topicData.tid);
+        socketHelpers.emitToUids('event:new_post', { posts: [result.postData] }, [caller.uid]);
+        socketHelpers.emitToUids('event:new_topic', result.topicData, [caller.uid]);
+        socketHelpers.notifyNew(caller.uid, 'newTopic', { posts: [result.postData], topic: result.topicData });
+        return result.topicData;
     });
 };
-
+topicsAPI.reply = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!data || !data.tid || (meta.config.minimumPostLength !== 0 && !data.content)) {
+            throw new Error('[[error:invalid-data]]');
+        }
+        const payload = Object.assign({}, data);
+        apiHelpers.setDefaultPostData(caller, payload);
+        yield meta.blacklist.test(caller.ip);
+        const shouldQueue = yield posts.shouldQueue(caller.uid, payload);
+        if (shouldQueue) {
+            return yield posts.addToQueue(payload);
+        }
+        const postData = yield topics.reply(payload); // postData seems to be a subset of postObj, refactor?
+        const postObj = yield posts.getPostSummaryByPids([postData.pid], caller.uid, {});
+        const result = {
+            posts: [postData],
+            'reputation:disabled': meta.config['reputation:disabled'] === 1,
+            'downvote:disabled': meta.config['downvote:disabled'] === 1,
+        };
+        user.updateOnlineUsers(caller.uid);
+        if (caller.uid) {
+            socketHelpers.emitToUids('event:new_post', result, [caller.uid]);
+        }
+        else if (caller.uid === 0) {
+            websockets.in('online_guests').emit('event:new_post', result);
+        }
+        socketHelpers.notifyNew(caller.uid, 'newPost', result);
+        return postObj[0];
+    });
+};
+topicsAPI.delete = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('delete', 'event:topic_deleted', caller, {
+            tids: data.tids,
+        });
+    });
+};
+topicsAPI.restore = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('restore', 'event:topic_restored', caller, {
+            tids: data.tids,
+        });
+    });
+};
 // added resolve function API
-topicsAPI.resolve = async function (caller, data) {
-    await doTopicAction('resolve', 'event:topic_resolved', caller, {
-        tids: data.tids,
+topicsAPI.resolve = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('resolve', 'event:topic_resolved', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.purge = async function (caller, data) {
-    await doTopicAction('purge', 'event:topic_purged', caller, {
-        tids: data.tids,
+topicsAPI.purge = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('purge', 'event:topic_purged', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.pin = async function (caller, data) {
-    await doTopicAction('pin', 'event:topic_pinned', caller, {
-        tids: data.tids,
+topicsAPI.pin = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('pin', 'event:topic_pinned', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.unpin = async function (caller, data) {
-    await doTopicAction('unpin', 'event:topic_unpinned', caller, {
-        tids: data.tids,
+topicsAPI.unpin = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('unpin', 'event:topic_unpinned', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.lock = async function (caller, data) {
-    await doTopicAction('lock', 'event:topic_locked', caller, {
-        tids: data.tids,
+topicsAPI.lock = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('lock', 'event:topic_locked', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.unlock = async function (caller, data) {
-    await doTopicAction('unlock', 'event:topic_unlocked', caller, {
-        tids: data.tids,
+topicsAPI.unlock = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield doTopicAction('unlock', 'event:topic_unlocked', caller, {
+            tids: data.tids,
+        });
     });
 };
-
-topicsAPI.follow = async function (caller, data) {
-    await topics.follow(data.tid, caller.uid);
+topicsAPI.follow = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield topics.follow(data.tid, caller.uid);
+    });
 };
-
-topicsAPI.ignore = async function (caller, data) {
-    await topics.ignore(data.tid, caller.uid);
+topicsAPI.ignore = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield topics.ignore(data.tid, caller.uid);
+    });
 };
-
-topicsAPI.unfollow = async function (caller, data) {
-    await topics.unfollow(data.tid, caller.uid);
+topicsAPI.unfollow = function (caller, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield topics.unfollow(data.tid, caller.uid);
+    });
 };
